d7f25eda0ecc24c3e3fb000b56281859d2286df3
Jean-Philippe Pierre <jean-philippepierre@localhost.localdomain>
2019-05-02 18:26:10 -0400

    Updated redirect.html so that user has to answer questions to redirect

--- src/redirect.js
+++ src/redirect.js
@@ -1,8 +1,21 @@
 function sendBack() {
-	var message = {
-		type: "sendBack"
+
+	var answer1 = document.getElementById("text1").value;
+	var answer2 = document.getElementById("text2").value;
+	var answer3 = document.getElementById("text3").value;
+	var answer4 = document.getElementById("text4").value;
+
+	//all the answers need to be filled out.
+	if(answer1 != "" && answer2 != "" && answer3 != "" && answer4 != "") {
+
+		var message = {
+			type: "sendBack"
+		}
+		browser.runtime.sendMessage(message);
+	
+	} else {
+		document.getElementById("error").textContent = "Not all the questions have been answered.";
 	}
-	browser.runtime.sendMessage(message);
 }
 
 document.getElementById("button").addEventListener("click", sendBack);
