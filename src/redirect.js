3e745b6e26e14f8f51a2da9f89f13591ed865277
Jean-Philippe Pierre <jean-philippepierre@localhost.localdomain>
2019-06-03 12:01:12 -0400

    Changed UI to make it less tedious

--- src/redirect.js
+++ src/redirect.js
@@ -1,6 +1,6 @@
 function sendBack() {
 
-	var answer1 = document.getElementById("text1").value;
+	/*var answer1 = document.getElementById("text1").value;
 	var answer2 = document.getElementById("text2").value;
 	var answer3 = document.getElementById("text3").value;
 	var answer4 = document.getElementById("text4").value;
@@ -15,7 +15,20 @@ function sendBack() {
 	
 	} else {
 		document.getElementById("error").textContent = "Not all the questions have been answered.";
+	}*/
+
+	var answer = document.getElementById("answer").value;
+
+	//if correct, get out of the page.
+	if(answer == "Continue") {
+		var message = {
+			type: "sendBack"
+		}
+		browser.runtime.sendMessage(message);
+	} else {
+		document.getElementById("error").textContent = "Input  \"Continue\" to continue to the website.";
 	}
+
 }
 
 document.getElementById("button").addEventListener("click", sendBack);
